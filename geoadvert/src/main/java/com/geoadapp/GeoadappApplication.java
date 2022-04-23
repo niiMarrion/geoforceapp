b72aaa1b4a880daf63a406c594fdc3b5518f77ca
niiMarrion <jephtahaddison@gmail.com>
2022-04-24 00:30:25 +0100

    App structure consisting and configuration initialisation

---
+++ geoadvert/src/main/java/com/geoadapp/GeoadappApplication.java
@@ -0,0 +1,13 @@
+package com.geoadapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoadappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeoadappApplication.class, args);
+    }
+
+}
---
+++ geoadvert/src/test/java/com/geoadapp/GeoadappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geoadapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoadappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
